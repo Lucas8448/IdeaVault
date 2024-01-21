21148cf6a6141511e3559cfa0d50642a8457856a
Lucas Bateson <lucas.bateson@gmail.com>
2024-01-21 17:25:12 +0100

    First code

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "IdeaVault",
+  description:
+    "Private collaborative brainstorming",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/Lucas8448/IdeaVault",
+    docs: "https://ui.shadcn.com",
+  },
+}
