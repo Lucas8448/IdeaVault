03adddfaa0d4b0e3947a638c440f67c423dea954
Lucas Bateson <lucas.bateson@gmail.com>
2024-02-15 12:16:30 +0100

    Add basic portal page

---
+++ app/portal/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import React, { useEffect } from "react"
+import { useAuth } from "@/components/context/Auth"
+
+export default function Portal() {
+  const auth = useAuth()
+  const router = useRouter()
+  
+  useEffect(() => {
+    if (auth.currentUser == null) {
+      router.push('/login')
+    }
+  })
+  
+  return (
+    <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Welcome, {auth.currentUser.displayName}
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          We are focused on creating a platform for users to collaborate in brainstorming anonymously with others, where only the teacher or host can see who has submitted ideas.
+        </p>
+      </div>
+    </section>
+  )
+}
