358d5287a7702837ab6e274a463f5ee274b98a1e
Lucas Bateson <lucas.bateson@gmail.com>
2024-02-19 00:25:09 +0100

    Add buttons on portal page

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -2,7 +2,17 @@
 
 import { useRouter } from "next/navigation"
 import React, { useMemo } from "react"
+import Link from "next/link"
 import { useAuth } from "@/components/context/Auth"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Page() {
   const auth = useAuth()
@@ -16,13 +26,23 @@ export default function Page() {
   
   return (
     <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <div className="flex flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Welcome, {auth.currentUser && auth.currentUser.displayName}
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          We are focused on creating a platform for users to collaborate in brainstorming anonymously with others, where only the teacher or host can see who has submitted ideas.
-        </p>
+        <Card className="mx-2 my-2">
+          <CardHeader>
+            <CardTitle>Get started</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Button variant="default">
+              <Link href="/portal/session/join">Join a session</Link>
+            </Button>
+            <Button variant="outline" className="mx-2">
+              <Link href="/portal/session/new">Create new session</Link>
+            </Button>
+          </CardContent>
+        </Card>
       </div>
     </section>
   )
