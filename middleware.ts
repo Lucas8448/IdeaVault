721f6654d2c95b3c0b9a25920e717da80262059e
Lucas Bateson <lucas.bateson@gmail.com>
2024-02-21 21:26:45 +0100

    Use regex to just block all portal subpages

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/vision", "/login", "/contact"],
+  publicRoutes: ["((?!^/portal).*)"]
 });
 
 export const config = {
