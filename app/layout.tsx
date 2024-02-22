06d0830adf1a7d0c3d40b1aa174a7dc3048976eb
Lucas Bateson <lucas.bateson@gmail.com>
2024-02-22 14:07:14 +0100

    Remove dark mode to fix build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css"
 import { Metadata } from "next"
 import React from "react"
 import { ClerkProvider } from '@clerk/nextjs'
-import {dark} from "@clerk/themes";
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -37,7 +36,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
-      <ClerkProvider appearance={dark}>
+      <ClerkProvider>
         <html lang="en" suppressHydrationWarning>
           <head />
           <body
